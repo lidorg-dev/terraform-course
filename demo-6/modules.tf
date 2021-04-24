4521f4b81d36776c3a89cd37cc1f0b8e490948a7
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2021-04-24 17:40:36 +0300

    Update modules.tf

--- demo-6/modules.tf
+++ demo-6/modules.tf
@@ -1,5 +1,5 @@
 module "consul" {
-  source   = "github.com/wardviaene/terraform-consul-module.git?ref=terraform-0.12"
+  source   = "https://github.com/lidorg-dev/terraform-consul-module"
   key_name = aws_key_pair.mykey.key_name
   key_path = var.PATH_TO_PRIVATE_KEY
   region   = var.AWS_REGION
