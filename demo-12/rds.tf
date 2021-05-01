827873558f802ef649db7ab3c0d09598f56f16f9
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2021-05-01 20:36:01 +0300

    Update rds.tf

--- demo-12/rds.tf
+++ demo-12/rds.tf
@@ -18,7 +18,7 @@ resource "aws_db_parameter_group" "mariadb-parameters" {
 resource "aws_db_instance" "mariadb" {
   allocated_storage       = 100 # 100 GB of storage, gives us more IOPS than a lower number
   engine                  = "mariadb"
-  engine_version          = "10.1.14"
+  engine_version          = "10.4.13"
   instance_class          = "db.t2.small" # use micro if you want to use the free tier
   identifier              = "mariadb"
   name                    = "mariadb"
